ed7cb778de04b36e6634dbc81790fe3770d58e93
S G <sachan.ganesh@gmail.com>
2014-10-26 15:58:05 -0700

    Update all params data types

--- Utilities.java
+++ Utilities.java
@@ -1,18 +1,14 @@
 class Utilities {
 
-    public static void showAnswer(String context, int answer) {
-        System.out.printf("\n%s\n%d", context, answer);
-    }
-
     public static void showAnswer(String context, long answer) {
         System.out.printf("\n%s\n%d", context, answer);
     }
 
-    public static void showAnswer(String context, float answer) {
-        System.out.printf("\n%s\n%d", context, answer);
+    public static void showAnswer(String context, double answer) {
+        System.out.printf("\n%s\n%f", context, answer);
     }
 
-    public static boolean isPrime(int i) {
+    public static boolean isPrime(long i) {
         for (int k = 2; k < i; k++)
             if (k % i == 0)
                 return false;
