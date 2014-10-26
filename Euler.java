eebb8159184860669ea2be89dde24a46d06cd064
S G <sachan.ganesh@gmail.com>
2014-10-26 14:11:39 -0700

    Completed problem 1

--- Euler.java
+++ Euler.java
@@ -25,6 +25,10 @@ public class Euler {
                 System.out.print("\n\n");
                 System.out.print("Please choose an Euler problem: ");
                 response = input.nextLine();
+                if (response.toLowerCase().equals("q")) {
+                    System.out.printf("\nThank you!\n\n");
+                    System.exit(0);
+                }
     			try {
     				choice = Integer.parseInt(response);
                 } catch (NumberFormatException e) {
@@ -36,9 +40,24 @@ public class Euler {
     }
 
     void pickSolution(int i) {
-        System.out.print("\n\n");
-        System.out.println(i);
-        System.out.print("\n\n");
+        System.out.print("\n");
+        switch (i) {
+            case 1: one(); break;
+            default: System.out.println("Problem has not been solved yet."); break;
+        }
+    }
+
+    void one() {
+        final int cap = 1000;
+        int i = 3, sum = 0;
+        do {
+            if (i % 3 == 0 || i % 5 == 0)
+                sum += i++;
+            else
+                i++;
+        } while (i < cap);
+        System.out.println("Sum of all the multiples of 3 or 5 below 1000.");
+        System.out.printf("\n%d", sum);
     }
 
 }
