389eaba2946ad62f72c9f6b18b9e39ac4cff7ee2
S G <sachan.ganesh@gmail.com>
2014-10-30 21:22:25 -0700

    Changed showAnswer method params; no overloading :bowtie:

--- Utilities.java
+++ Utilities.java
@@ -1,13 +1,8 @@
 class Utilities {
 
-    public static void showAnswer(String context, long answer) {
+    public static void showAnswer(String context, Number answer) {
         System.out.printf("\n%s\n%d", context, answer);
     }
-
-    public static void showAnswer(String context, double answer) {
-        System.out.printf("\n%s\n%f", context, answer);
-    }
-
     public static boolean isPrime(long prime) {
         for(int i = 2; i <= Math.sqrt(prime); i++)
             if (prime % i == 0)
