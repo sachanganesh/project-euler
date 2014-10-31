3c799b26412f8630aae2376d04dce51c2bdb68a5
S G <sachan.ganesh@gmail.com>
2014-10-30 21:40:41 -0700

    Added annotations to utility methods :no_mouth:

--- Utilities.java
+++ Utilities.java
@@ -1,15 +1,27 @@
 class Utilities {
 
+    /*
+    * Displays context for the problem and the corresponding answer to the problem.
+    */
     public static void showAnswer(String context, Number answer) {
         System.out.printf("\n%s\n%d", context, answer);
     }
-    public static boolean isPrime(long prime) {
+
+    /*
+    * Determines if a number is prime.
+    * Returns true if it is prime; false if it is not prime.
+    */
+    public static boolean isPrime(Number prime) {
         for(int i = 2; i <= Math.sqrt(prime); i++)
             if (prime % i == 0)
                 return false;
         return true;
     }
 
+    /*
+    * Determines if a String value of a number is palindromic.
+    * Returns true if it is palindromic; false if it is not palindromic.
+    */
     public static boolean isPalindrome(String palindrome) {
         String fragment1 = palindrome.substring(0, palindrome.length() / 2),
         fragment2 = palindrome.substring(palindrome.length() / 2),
@@ -19,8 +31,13 @@ class Utilities {
         return fragment1.equals(reverseFragment);
     }
 
-    public static boolean isPalindrome(long palindrome) {
-        return isPalindrome("" + palindrome);
+
+    /*
+    * Determines if a String value of a number is palindromic
+    * by calling overloaded method with String param.
+    */
+    public static boolean isPalindrome(Number palindrome) {
+        return isPalindrome("" + (long)palindrome);
     }
 
 }
