4a341b000e3fc5ff6255f49098b01a0d6c6d6f43
S G <sachan.ganesh@gmail.com>
2014-11-01 12:12:18 -0700

    Changed toString erroneous call to String.valueOf()

--- Utilities.java
+++ Utilities.java
@@ -49,7 +49,7 @@ class Utilities {
     * by calling overloaded method with String param.
     */
     public static boolean isPalindrome(Number palindrome) {
-        return isPalindrome(Long.toString(palindrome));
+        return isPalindrome(String.valueOf(palindrome));
     }
 
 }
